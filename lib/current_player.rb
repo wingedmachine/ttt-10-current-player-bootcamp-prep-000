9f88c1d6da097d5a2f785c7fd29b866e20eccd35
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-18 01:44:35 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,13 @@
 def turn_count(board)
   count = 0
-  board.each do |space| 
-    if(space != " ") 
-      count += 1 
+  board.each do |space|
+    if space != " "
+      count += 1
     end
   end
   count
+end
+
+def current_player
+  
 end
